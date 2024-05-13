72692db31317677fedc8f6377d57d5acd5daf4d4
Rahul Singh <rahul.singh@juspay.in>
2024-05-13 11:49:49 +0530

    Added the  maybe ttl support

--- src/Database/RocksDB/Base.hs
+++ src/Database/RocksDB/Base.hs
@@ -409,13 +409,15 @@ createColumnFamily (DB { rocksDB }) config cfNameStr =
         withOptions config $ \opts ->
           c_rocksdb_create_column_family rocksDB opts cfName err
 
-createColumnFamilyWithTtl :: MonadUnliftIO m => DB -> Config -> String -> Int -> m ColumnFamily
-createColumnFamilyWithTtl (DB { rocksDB }) config cfNameStr ttl =
+createColumnFamilyWithTtl :: MonadUnliftIO m => DB -> Config -> String -> Maybe Int -> m ColumnFamily
+createColumnFamilyWithTtl (DB { rocksDB }) config cfNameStr maybeTtl =
   liftIO $
     withCString cfNameStr $ \cfName -> do
       throwIfErr "create_column_family" $ \err ->
         withOptions config $ \opts ->
-          c_rocksdb_create_column_family_with_ttl rocksDB opts cfName (toEnum ttl) err
+          case maybeTtl of
+            Just ttl -> c_rocksdb_create_column_family_with_ttl rocksDB opts cfName (toEnum ttl) err
+            _ -> c_rocksdb_create_column_family rocksDB opts cfName err
 
 dropColumnFamily :: MonadUnliftIO m => DB -> ColumnFamily -> m ()
 dropColumnFamily (DB { rocksDB }) cf =
